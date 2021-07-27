eb40d315d577c742bdccc3cd43f9d9fae6f70ae5
Pravin Malvi <Pravin@Valasys.local>
2021-07-28 01:37:55 +0530

    logo change

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,28 +1,29 @@
 <!-- [ Header ] start -->
 <header class="navbar pcoded-header navbar-expand-lg navbar-light">
     <div class="m-header">
-        <a class="mobile-menu" id="mobile-collapse1" href="#!"><span></span></a>
-        <a href="index.html" class="b-brand">
+        <a class="mobile-menu" id="mobile-collapse1" href="javascript:void(0)"><span></span></a>
+        <a href="javascript:void(0)" class="b-brand">
             <div class="b-bg">
                 <i class="feather icon-trending-up"></i>
             </div>
             <span class="b-title">Datta Able</span>
         </a>
     </div>
-    <a class="mobile-menu" id="mobile-header" href="#!">
+    <a class="mobile-menu" id="mobile-header" href="javascript:void(0)">
         <i class="feather icon-more-horizontal"></i>
     </a>
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
-            <li><a href="#!" class="full-screen" onclick="javascript:toggleFullScreen()"><i class="feather icon-maximize"></i></a></li>
+            <li><a href="javascript:void(0)" class="full-screen" onclick="javascript:toggleFullScreen()"><i class="feather icon-maximize"></i></a></li>
             
         </ul>
         <ul class="navbar-nav ml-auto">
             
             <li>
                 <div class="dropdown drp-user">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="icon feather icon-settings"></i>
+                    <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="icon feather icon-log-out"></i>
+                        
                     </a>
                     <div class="dropdown-menu dropdown-menu-right profile-notification">
                         <div class="pro-head">
@@ -34,9 +35,6 @@
                             </a>
                         </div>
                         <ul class="pro-body">
-                            {{-- <li><a href="#!" class="dropdown-item"><i class="feather icon-settings"></i> Settings</a></li>
-                            <li><a href="#!" class="dropdown-item"><i class="feather icon-user"></i> Profile</a></li>
-                            <li><a href="message.html" class="dropdown-item"><i class="feather icon-mail"></i> My Messages</a></li> --}}
                             <li>
                                 <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();"><i class="feather icon-lock"></i> Sign Out</a>
